e09ba07cdfee74ca20641402b553762e0fed8e70
fabio lima <fabio.lima19997@gmail.com>
2025-01-29 00:53:50 -0300

    Fix mapped user response

--- src/Application/Queries/UserQueries/GetUserByIdQuery.cs
+++ src/Application/Queries/UserQueries/GetUserByIdQuery.cs
@@ -1,12 +1,10 @@
-﻿using EmployeeUnitManagementApi.src.Domain.Common;
-
-namespace EmployeeUnitManagementApi.src.Application.Queries.UserQueries
+﻿namespace EmployeeUnitManagementApi.src.Application.Queries.UserQueries
 {
 
     /// <summary>
     /// Represents a query to get a user by their ID.
     /// </summary>
-    public sealed class GetUserByIdQuery : BaseEntity
+    public sealed class GetUserByIdQuery
     {
         /// <summary>
         /// Gets the unique identifier for the user.
@@ -27,6 +25,34 @@ namespace EmployeeUnitManagementApi.src.Application.Queries.UserQueries
         /// Gets the status of the user (Active, Inactive).
         /// </summary>
         public string? Status { get; }
+
+
+        /// <summary>
+        /// Gets the creation date and time of the entity.
+        /// </summary>
+        public DateTimeOffset CreatedAt { get; }
+
+        /// <summary>
+        /// Gets or sets the last updated date and time of the entity.
+        /// </summary>
+        public DateTimeOffset? UpdatedAt { get; } = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetUserByIdQuery"/> class.
+        /// </summary>
+        /// <param name="id">The unique identifier for the user.</param>
+        /// <param name="email">The email of the user.</param>
+        /// <param name="role">The role of the user (Admin, User).</param>
+        /// <param name="status">The status of the user (Active, Inactive).</param>
+        public GetUserByIdQuery(Guid id, string? email, string? role, string? status, DateTimeOffset createdAt, DateTimeOffset? updatedAt)
+        {
+            Id = id;
+            Email = email;
+            Role = role;
+            Status = status;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
 
     }
 
